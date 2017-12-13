6923664fae25f9526d8bac10e3c92090d7fc2201
kaka <vn503024@gmail.com>
2017-12-13 13:37:56 +0800

    Add: 增加圖片識別功能(Use Tensorflow-mobile)

--- src/Home.js
+++ src/Home.js
@@ -1,46 +1,67 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { TfImageRecognition } from 'react-native-tensorflow';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class Test extends Component {
 
-export default class Home extends Component<{}> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit Home.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
+    constructor() {
+      super()
+      this.image = require('./assets/dumbbell.jpg');
+      this.state = {result: ""}
+    }
+
+    componentDidMount() {
+      this.recognizeImage()
+    }
+
+    async recognizeImage() {
+
+      try {
+        const tfImageRecognition = new TfImageRecognition({
+          model:require('./assets/tensorflow_inception_graph.pb'),
+          labels: require('./assets/tensorflow_labels.txt')
+        })
+
+        const results = await tfImageRecognition.recognize({
+          image: this.image
+        })
+
+        const resultText = `Name: ${results[0].name} - Confidence: ${results[0].confidence}`
+        this.setState({result: resultText})
+      } catch(err) {
+        alert(err)
+      }
+    }
+
+    render() {
+      return (
+        <View style={styles.container}>
+          <Text style={styles.welcome}>
+            Welcome to React Native!
+          </Text>
+            <Image source={this.image} style={styles.image} />
+          <Text style={styles.results}>
+            {this.state.result}
+          </Text>
+        </View>
+      );
+    }
   }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#F5FCFF',
+    },
+    results: {
+      textAlign: 'center',
+      color: '#333333',
+      marginBottom: 5,
+    },
+    image: {
+      width: 150,
+      height: 100
+    },
+  });
