7c9901fc31be2aa45b016f7354260f4254a88e7d
kaka <vn503024@gmail.com>
2017-12-13 11:30:02 +0800

    Add: 新增TabNavigator 功能

--- src/app.ios.js
+++ src/app.ios.js
@@ -1,60 +1,21 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import { AppRegistry } from 'react-native';
+import { StackNavigator, TabNavigator } from 'react-navigation';
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import Home from './Home';
+import Home2 from './Home2';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+const MainHomeNavigator = TabNavigator({
+  Home1: { screen: Home },
+  Home2: { screen: Home2 },
 });
 
-export default class App extends Component<{}> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
-}
+MainHomeNavigator.navigationOptions = {
+  title: 'Home'
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+const App = StackNavigator({
+  Home: { screen: MainHomeNavigator},
+  //Profile: { screen: ViewPhotos},
 });
 
 AppRegistry.registerComponent('kakaApp', () => App);
